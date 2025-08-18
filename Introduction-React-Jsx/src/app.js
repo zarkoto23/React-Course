a357a84105bedd20de87bf1a64defd4e06ce7241
Svetozar Stoev <zarkoto23@gmail.com>
2025-08-18 18:46:07 +0300

    compare react vs jsx syntax

--- Introduction-React-Jsx/src/app.js
+++ Introduction-React-Jsx/src/app.js
@@ -3,10 +3,16 @@ const rootDomEl=document.getElementById('root')
 
 const reactRoot=ReactDOM.createRoot(rootDomEl)
 
-
+//withoutJSX
 const headingReactEl=React.createElement('h1',{},'hello from jsx')
 const subHeadingReactEl=React.createElement('h2',{id:'sub-header'}, 'react best framework')
 const headerSectionReactEl=React.createElement('header',{},headingReactEl,subHeadingReactEl)
 
+//withJSX
+const headerSectionJsxtEl=(<header>
+    <h1>hello from jsssx</h1>
+    <h2 id="sub-header">the best jsssx</h2>
+    <p>lorem10  asdfasfas</p>
+</header>)
 
-reactRoot.render(headerSectionReactEl)
+reactRoot.render(headerSectionJsxtEl)

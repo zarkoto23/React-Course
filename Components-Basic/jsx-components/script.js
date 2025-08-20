502005d4c87391e5c1cc9a99403d10d85372c3f5
Svetozar Stoev <zarkoto23@gmail.com>
2025-08-20 14:10:45 +0300

    fragment

--- Components-Basic/jsx-components/script.js
+++ Components-Basic/jsx-components/script.js
@@ -10,9 +10,11 @@ function Header(){
 const headerReactEl=React.createElement('h1',null,'react components!')
 const subHeaderReactEl=React.createElement('h2',{className:'sub-header'}, 'components awesome')
 
-const divReactEl=React.createElement('div',null, headerReactEl, subHeaderReactEl)
+// const divReactEl=React.createElement('div',null, headerReactEl, subHeaderReactEl)
 
-return divReactEl
+const fragmentEl=React.createElement(React.Fragment, null,headerReactEl, subHeaderReactEl)
+
+return fragmentEl
 }
 
 

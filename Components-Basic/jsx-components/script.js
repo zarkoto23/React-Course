4a787e351eec55f6c949f17b513552feb5cfeae0
Svetozar Stoev <zarkoto23@gmail.com>
2025-08-20 14:17:32 +0300

    compose components

--- Components-Basic/jsx-components/script.js
+++ Components-Basic/jsx-components/script.js
@@ -1,22 +1,45 @@
-const rootDomEl=document.getElementById('root')
+const rootDomEl = document.getElementById("root");
 
-const rootReactEl=ReactDOM.createRoot(rootDomEl)
+const rootReactEl = ReactDOM.createRoot(rootDomEl);
 
 // const headerReactEl=React.createElement('h1',null,'react components')
 
-
 //functional component is function that returns react element!
-function Header(){
-const headerReactEl=React.createElement('h1',null,'react components!')
-const subHeaderReactEl=React.createElement('h2',{className:'sub-header'}, 'components awesome')
+function Header() {
+  const headerReactEl = React.createElement("h1", null, "react components!");
+  const subHeaderReactEl = React.createElement(
+    "h2",
+    { className: "sub-header" },
+    "components awesome"
+  );
 
-// const divReactEl=React.createElement('div',null, headerReactEl, subHeaderReactEl)
+  // const divReactEl=React.createElement('div',null, headerReactEl, subHeaderReactEl)
 
-const fragmentEl=React.createElement(React.Fragment, null,headerReactEl, subHeaderReactEl)
+  const fragmentEl = React.createElement(
+    React.Fragment,
+    null,
+    headerReactEl,
+    subHeaderReactEl
+  );
 
-return fragmentEl
+  return fragmentEl;
 }
 
+function Body() {
+  return React.createElement(
+    "main",
+    null,
+    React.createElement("p", null, "lorem10")
+  );
+}
 
+function Main() {
+  return React.createElement(
+    React.Fragment,
+    null,
+    React.createElement(Header),
+    React.createElement(Body)
+  );
+}
 
-rootReactEl.render(React.createElement(Header))
+rootReactEl.render(React.createElement(Main));

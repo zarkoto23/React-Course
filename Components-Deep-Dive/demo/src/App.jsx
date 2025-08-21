696a11790583bd91c234092cf6be0f3c9804e251
Svetozar Stoev <zarkoto23@gmail.com>
2025-08-21 17:03:40 +0300

    update reference type

--- Components-Deep-Dive/demo/src/App.jsx
+++ Components-Deep-Dive/demo/src/App.jsx
@@ -1,33 +1,34 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [movies, setMovies]=useState([
+    'The Matrix',
+    'Man of Steel',
+    'The Case of Christ',
+    'Lord of the Rings',
+    'Spiderman'
+  ])
+
+  const removeFirstHandler=()=>{
+    movies.shift()
+
+    //! when update reference type, we need new reference for updating correctly
+    setMovies([...movies])
+    
+  }
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h2>Movie list</h2>
+    <ul>
+      {movies.map(movie=><li key={movie}>{movie}</li>)}
+    </ul>
+
+    <button onClick={removeFirstHandler}>remove first</button>
     </>
   )
 }

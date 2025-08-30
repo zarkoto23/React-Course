6390ab31e47dfa92b4409a01d6bdc22ab1605d56
Svetozar Stoev <zarkoto23@gmail.com>
2025-08-30 18:34:36 +0300

    add change status feature

--- todo-list/client/src/App.jsx
+++ todo-list/client/src/App.jsx
@@ -1,102 +1,79 @@
-
-import { useEffect, useState } from 'react'
-import './App.css'
-import Item from './Item'
+import { useEffect, useState } from "react";
+import "./App.css";
+import Item from "./Item";
 
 function App() {
+  const [todos, setTodos] = useState([]);
 
-  const [todos, setTodos]=useState([])
+  useEffect(() => {
+    fetch("http://localhost:3030/jsonstore/todos")
+      .then((res) => res.json())
+      .then((data) => {
+        setTodos(Object.values(data));
+      })
+      .catch((err) => console.log(err.message));
+  }, []);
 
-  useEffect(()=>{
-
-    fetch('http://localhost:3030/jsonstore/todos')
-    .then(res=>res.json())
-    .then(data=>{
-        setTodos(Object.values(data))
-
-    })
-    .catch(err=>console.log(err.message))
-
-    
-
-  },[])
-
-
-
-
-
-
-
+  function toggleStatus(id) {
+    setTodos((state) =>
+      state.map((todo) =>
+        todo._id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
+      )
+    );
+  }
 
   return (
-  <>
-  <meta charSet="UTF-8" />
-  <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  {/* <link rel="stylesheet" href="./static/styles.css" /> */}
-  <title>Todo List</title>
+    <>
+      <meta charSet="UTF-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      {/* <link rel="stylesheet" href="./static/styles.css" /> */}
+      <title>Todo List</title>
 
+      <header className="navigation-header">
+        <span className="navigation-logo">
+          <img src="/todo-icon.png" alt="todo-logo" />
+        </span>
+        <span className="spacer" />
+        <span className="navigation-description">Todo List</span>
+      </header>
 
-
-  <header className="navigation-header">
-    <span className="navigation-logo">
-      <img src="/todo-icon.png" alt="todo-logo" />
-    </span>
-    <span className="spacer" />
-    <span className="navigation-description">Todo List</span>
-  </header>
-
-
-
-
-  <main className="main">
-
-
-
-    <section className="todo-list-container">
-      <h1>Todo List</h1>
-      <div className="add-btn-container">
-        <button className="btn">+ Add new Todo</button>
-      </div>
-      <div className="table-wrapper">
-        {/*<div className="loading-container">
+      <main className="main">
+        <section className="todo-list-container">
+          <h1>Todo List</h1>
+          <div className="add-btn-container">
+            <button className="btn">+ Add new Todo</button>
+          </div>
+          <div className="table-wrapper">
+            {/*<div className="loading-container">
           <div className="loading-spinner">
             <span className="loading-spinner-text">Loading</span>
           </div>
         </div> */}
 
+            <table className="table">
+              <thead>
+                <tr>
+                  <th className="table-header-task">Task</th>
+                  <th className="table-header-status">Status</th>
+                  <th className="table-header-action">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {todos.map((todo) => (
+                  <Item key={todo._id} todo={todo} onToggle={toggleStatus} />
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </section>
+      </main>
 
-
-
-        <table className="table">
-          <thead>
-            <tr>
-              <th className="table-header-task">Task</th>
-              <th className="table-header-status">Status</th>
-              <th className="table-header-action">Action</th>
-            </tr>
-          </thead>
-          <tbody>
-
-
-
-
-            {todos.map(todo=><Item key={todo._id} todo={todo}/>)}
-
-          </tbody>
-        </table>
-      </div>
-    </section>
-  </main>
-
-
-  <footer className="footer">
-    <p>Copyright © designed by Mihail Valkov</p>
-  </footer>
-</>
-
-      
-  )
+      <footer className="footer">
+        <p>Copyright © designed by Mihail Valkov</p>
+      </footer>
+    </>
+  );
 }
 
-export default App
+export default App;
--- todo-list/client/src/Item.jsx
+++ todo-list/client/src/Item.jsx
@@ -1,16 +1,23 @@
-import styles from './Item.module.css'
+import styles from "./Item.module.css";
 
-export default function Item(props){
+export default function Item(props) {
+  let compl = [styles.todo];
+  if (props.todo.isCompleted) {
+    compl.push(styles["is-completed"]);
+  }
 
-
-    return(
-              <tr className={styles.todo}>
-              <td>{props.todo.text}</td>
-              <td>{props.todo.isCompleted ? 'Complete':'Incomplete'}</td>
-              <td className="todo-action">
-                <button className="btn todo-btn">Change status</button>
-              </td>
-            </tr>
-
-    )
+  return (
+    <tr className={compl.join(" ")}>
+      <td>{props.todo.text}</td>
+      <td>{props.todo.isCompleted ? "Complete" : "Incomplete"}</td>
+      <td className="todo-action">
+        <button
+          onClick={() => props.onToggle(props.todo._id)}
+          className="btn todo-btn"
+        >
+          Change status
+        </button>
+      </td>
+    </tr>
+  );
 }

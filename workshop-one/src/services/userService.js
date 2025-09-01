53aa9336cc6a85bf6c216ac09759f99b8bc0779d
Svetozar Stoev <zarkoto23@gmail.com>
2025-09-01 16:56:40 +0300

    update userData format send to the server

--- workshop-one/src/services/userService.js
+++ workshop-one/src/services/userService.js
@@ -1,24 +1,27 @@
-const baseUrl='http://localhost:3030/jsonstore/users'
+const baseUrl = "http://localhost:3030/jsonstore/users";
 
-export default{
+export default {
+  async getAll() {
+    const response = await fetch(baseUrl);
+    const result = await response.json();
+    const users = Object.values(result);
 
-    async getAll(){
-        const response=await fetch(baseUrl)
-        const result=await response.json()
-        const users=Object.values(result)
+    return users;
+  },
 
-        return users
-    },
+  async create(userData) {
+    const { country, city, street, streetNumber, ...postData } = userData;
+    postData.address = { country, city, street, streetNumber };
+    postData.createdAt = new Date().toISOString();
+    postData.updatedAt = new Date().toISOString();
 
-    async create(userData){
-        const response = await fetch(baseUrl,{
-            method:"POST",
-            headers:{"Content-Type":"application/json"},
-            body:JSON.stringify(userData)
+    const response = await fetch(baseUrl, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(postData),
+    });
+    const result = await response.json();
 
-        })
-        const result=await response.json()
-
-        return result
-    }
-}
+    return result;
+  },
+};

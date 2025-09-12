06776c114e65f1ae9f060594b0f380b083927384
Svetozar Stoev <zarkoto23@gmail.com>
2025-09-12 14:41:20 +0300

    refactor: extract CatalogItem component for better code organization

--- Routing/src/components/Catalog.jsx
+++ Routing/src/components/Catalog.jsx
@@ -1,3 +1,5 @@
+import CatalogItem from "./CatalogItem"
+
 const products = [
   {
     id: 1,
@@ -73,15 +75,9 @@ export default function Catalog() {
 
         <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
           {products.map((product) => (
-            <a key={product.id} href={product.href} className="group">
-              <img
-                alt={product.imageAlt}
-                src={product.imageSrc}
-                className="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-7/8"
-              />
-              <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
-              <p className="mt-1 text-lg font-medium text-gray-900">{product.price}</p>
-            </a>
+         
+            <CatalogItem product={product} key={product.id}/>
+
           ))}
         </div>
       </div>
---
+++ Routing/src/components/CatalogItem.jsx
@@ -0,0 +1,15 @@
+export default function CatalogItem({
+    product
+}) {
+  return(
+        <a key={product.id} href={} className="group">
+              <img
+                alt={product.imageAlt}
+                src={product.imageSrc}
+                className="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-7/8"
+              />
+              <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
+              <p className="mt-1 text-lg font-medium text-gray-900">{product.price}</p>
+            </a>
+)
+}

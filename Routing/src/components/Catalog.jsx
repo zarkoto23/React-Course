540a6cd99509b5094c845676567d098db3dcc2ba
Svetozar Stoev <zarkoto23@gmail.com>
2025-09-15 18:17:14 +0300

    add filter by search params

--- Routing/src/components/Catalog.jsx
+++ Routing/src/components/Catalog.jsx
@@ -1,5 +1,21 @@
+
+import {
+
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+} from '@headlessui/react'
+
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
+
+
+
+
+
 import { useEffect, useState } from "react"
 import CatalogItem from "./CatalogItem"
+import { Link, useSearchParams } from 'react-router'
 
 // const products = [
 //   {
@@ -68,8 +84,35 @@ import CatalogItem from "./CatalogItem"
 //   },
 // ]
 
+const sortOptions = [
+  { name: 'All', href: '/catalog', current: true },
+  { name: 'Best Rating', href: '/catalog?topRating=rating=4', current: false },
+  { name: 'popular', href: '/catalog?ratingCount=100', current: false },
+  { name: 'Price: Low to High', href: '/catalog?sortBy=price&dir=asc', current: false },
+  { name: 'Price: High to Low', href: '/catalog?sortBy=price&dir=desc', current: false },
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
 export default function Catalog() {
+  const [searchParams, setSearchParams] = useSearchParams()
+
+
   const [products, setProducts]=useState([])
+
+  const [displayProducts, setDisplayProducts]=useState([])
+
+
+
+
+  console.log(searchParams.get('sortBy'));
+  console.log(Object.fromEntries(searchParams));
+
+
+
+  
 
 useEffect(()=>{
 fetch('https://fakestoreapi.com/products')
@@ -79,14 +122,60 @@ fetch('https://fakestoreapi.com/products')
 })
 },[])
 
+useEffect(()=>{
+  const filter= Object.fromEntries(searchParams)
+  
+
+  if(filter.sortBy){
+    setDisplayProducts([...products].sort((p1,p2)=>filter.dir==='asc' ? p1.price - p2.price : p2.price-p1.price))
+  }else{
+    setDisplayProducts([...products])
+  }
+
+},[products, searchParams])
+
 
   return (
+
     <div className="bg-white">
+
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <h2 className="sr-only">Products</h2>
 
+        
+       <Menu as="div" className="relative inline-block text-left">
+                <MenuButton className="group inline-flex justify-center text-sm font-medium text-gray-700 hover:text-gray-900">
+                  Sort
+                  <ChevronDownIcon
+                    aria-hidden="true"
+                    className="-mr-1 ml-1 size-5 shrink-0 text-gray-400 group-hover:text-gray-500"
+                  />
+                </MenuButton>
+
+                <MenuItems
+                  transition
+                  className="absolute right-0 z-10 mt-2 w-40 origin-top-right rounded-md bg-white shadow-2xl ring-1 ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
+                >
+                  <div className="py-1">
+                    {sortOptions.map((option) => (
+                      <MenuItem key={option.name}>
+                        <Link
+                          to={option.href}
+                          className={classNames(
+                            option.current ? 'font-medium text-gray-900' : 'text-gray-500',
+                            'block px-4 py-2 text-sm data-focus:bg-gray-100 data-focus:outline-hidden',
+                          )}
+                        >
+                          {option.name}
+                        </Link>
+                      </MenuItem>
+                    ))}
+                  </div>
+                </MenuItems>
+              </Menu>
+
         <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-          {products.map((product) => (
+          {displayProducts.map((product) => (
          
             <CatalogItem product={product} key={product.id}/>
 

2a6447f4a9d4bb9763c2400a9221fae2fd80bd4a
Svetozar Stoev <zarkoto23@gmail.com>
2025-09-25 18:34:08 +0300

    add all pages and routes

--- work-shop-routing-forms-game-play/client/src/App.jsx
+++ work-shop-routing-forms-game-play/client/src/App.jsx
@@ -1,6 +1,10 @@
 import Header from "./components/Header/Header";
 import Home from "./components/home/Home";
 import { Routes, Route } from "react-router";
+import Login from "./components/login/login";
+import Register from "./components/register/register";
+import GameCatalog from "./components/game-catalog/GameCatalog";
+import GameCreate from "./components/game-create/GameCreate";
 
 function App() {
   return (
@@ -10,6 +14,11 @@ function App() {
       <main id="main-content">
         <Routes>
           <Route index element={<Home />} />
+          <Route path="/games" element={<GameCatalog />} />
+          <Route path="/games/create" element={<GameCreate />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          {/* <Route path="/games" element={<GameCatalog />} /> */}
         </Routes>
       </main>
     </div>

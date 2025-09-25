391787f6dea4a13c2124fca98444902f4e3d0940
Svetozar Stoev <zarkoto23@gmail.com>
2025-09-25 17:34:51 +0300

    add nav and home page

--- work-shop-routing-forms-game-play/client/src/App.jsx
+++ work-shop-routing-forms-game-play/client/src/App.jsx
@@ -1,15 +1,15 @@
-
+import Header from "./components/Header/Header";
+import Home from "./components/home/Home";
 function App() {
-
   return (
     <div id="box">
+      <Header />
 
-
-
-      </div>
-
-
-  )
+      <main id="main-content">
+        <Home />
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;

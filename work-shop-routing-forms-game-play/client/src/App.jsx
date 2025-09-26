ac8458cfe1acebe052ec3f64f52ff3f4d4e0fa10
Svetozar Stoev <zarkoto23@gmail.com>
2025-09-26 18:12:16 +0300

    add login handler with lifted state

--- work-shop-routing-forms-game-play/client/src/App.jsx
+++ work-shop-routing-forms-game-play/client/src/App.jsx
@@ -1,15 +1,24 @@
 import Header from "./components/Header/Header";
 import Home from "./components/home/Home";
 import { Routes, Route } from "react-router";
-import Login from "./components/login/login";
+import Login from "./components/login/Login";
 import Register from "./components/register/register";
 import GameCatalog from "./components/game-catalog/GameCatalog";
 import GameCreate from "./components/game-create/GameCreate";
 import GameDetails from "./components/game-details/GameDetails";
 import  GameEdit from "./components/game-edit/GameEdit";
+import { useState } from "react";
 
 
 function App() {
+
+  const[email, setEmail]=useState('')
+
+  const userLoginHandler=(email)=>{
+    setEmail(email)
+
+  }
+
   return (
     <div id="box">
       <Header />
@@ -19,7 +28,7 @@ function App() {
           <Route index element={<Home />} />
           <Route path="/games" element={<GameCatalog />} />
           <Route path="/games/create" element={<GameCreate />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<Login onLogin={userLoginHandler} />} />
           <Route path="/register" element={<Register />} />
           <Route path="/games/:gameId/details" element={<GameDetails />} />
           <Route path="/games/:gameId/edit" element={<GameEdit />} />
--- work-shop-routing-forms-game-play/client/src/components/login/login.jsx
+++ work-shop-routing-forms-game-play/client/src/components/login/login.jsx
@@ -1,7 +1,21 @@
-export default function Login() {
+import { Link, useNavigate } from "react-router"
+
+export default function Login({onLogin}) {
+
+    const nav=useNavigate()
+
+    const loginAction=(formData)=>{
+        const email=formData.get('email')
+        onLogin(email)
+
+        nav('/games')
+        
+    }
+
+
   return(
      <section id="login-page" className="auth">
-            <form id="login">
+            <form id="login" action={loginAction}>
 
                 <div className="container">
                     <div className="brand-logo"></div>
@@ -13,9 +27,9 @@ export default function Login() {
                     <input type="password" id="login-password" name="password"/>
                     <input type="submit" className="btn submit" value="Login"/>
                     <p className="field">
-                        <span>If you don't have profile click <a href="#">here</a></span>
+                        <span>If you don't have profile click <Link to="/register">here</Link></span>
                     </p>
-                </div>
+                </div> 
             </form>
         </section>
 )

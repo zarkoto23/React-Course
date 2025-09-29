225330ccdacd98b80588de14e453f2e6927ad4f3
Svetozar Stoev <zarkoto23@gmail.com>
2025-09-29 15:23:46 +0300

    ad hook/usefetch file

--- React-Hooks/src/components/ChatComponent.jsx
+++ React-Hooks/src/components/ChatComponent.jsx
@@ -1,27 +1,37 @@
-import React from 'react';
-import { Avatar, List } from 'antd';
+import React from "react";
+import { Avatar, List } from "antd";
+import Link from "antd/es/typography/Link";
 const data = [
   {
-    title: 'Ant Design Title 1',
+    title: "Ant Design Title 1",
   },
   {
-    title: 'Ant Design Title 2',
+    title: "Ant Design Title 2",
   },
   {
-    title: 'Ant Design Title 3',
+    title: "Ant Design Title 3",
   },
   {
-    title: 'Ant Design Title 4',
+    title: "Ant Design Title 4",
   },
 ];
 const ChatComponent = () => (
   <List
+    bordered={true}
+    size="large"
+    split={true}
+    header={<h1>ChatChe</h1>}
+    loadMore={"loAdMore"}
     itemLayout="horizontal"
     dataSource={data}
     renderItem={(item, index) => (
       <List.Item>
         <List.Item.Meta
-          avatar={<Avatar src={`https://api.dicebear.com/7.x/miniavs/svg?seed=${index}`} />}
+          avatar={
+            <Avatar
+              src={`https://api.dicebear.com/7.x/miniavs/svg?seed=${index}`}
+            />
+          }
           title={<a href="https://ant.design">{item.title}</a>}
           description="Ant Design, a design language for background applications, is refined by Ant UED Team"
         />

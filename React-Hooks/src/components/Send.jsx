4005411a8e0ae669749c8965a0709205395a9b53
Svetozar Stoev <zarkoto23@gmail.com>
2025-10-01 18:23:18 +0300

    send msg to the server and pop up success message

--- React-Hooks/src/components/Send.jsx
+++ React-Hooks/src/components/Send.jsx
@@ -1,5 +1,52 @@
+import { Button, Input, message } from "antd";
+import { UserOutlined } from "@ant-design/icons";
+
 export default function Send() {
-  return(
-    <h1>Sendf</h1>
-)
+  const [messageApi, contextHolder] = message.useMessage();
+
+  const formSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const msg = formData.get("msg");
+    console.log(msg);
+
+    await fetch("http://localhost:3030/jsonstore/messenger", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        author: "Ui",
+        content: msg,
+      }),
+    });
+
+    messageApi.open({
+      type: "success",
+      content: 'Success!',
+    });
+  };
+
+  return (
+    <>
+      {contextHolder}
+      <form onSubmit={formSubmit}>
+        <br />
+        <br />
+        <br />
+        <br />
+        <Input
+          size="large"
+          name="msg"
+          placeholder="username"
+          prefix={<UserOutlined />}
+        />
+        <br />
+        <br />
+        <br />
+        <br />
+        <Button type="primary" htmlType="submit">
+          SendMessage
+        </Button>
+      </form>
+    </>
+  );
 }

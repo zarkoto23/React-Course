ab1eaa57e2625f66761996107e12109078d0c66c
Svetozar Stoev <zarkoto23@gmail.com>
2025-10-03 16:15:27 +0300

    refactor useForm hook for improved readability

--- React-Hooks/src/hooks/useForm.js
+++ React-Hooks/src/hooks/useForm.js
@@ -4,11 +4,12 @@ export default function useForm(callbackHandler,initialValues){
 
     const [values, setValues]=useState(initialValues)
 
-
+ 
     const changeHandler=(e)=>{
         setValues(state=>({...state, [e.target.name]:e.target.value}))
+        
     }
-
+    
     const submitHandler=(e)=>{
         e.preventDefault()
         callbackHandler(values)

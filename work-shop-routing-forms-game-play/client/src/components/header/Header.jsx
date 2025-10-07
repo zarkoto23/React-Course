676717cab1974e165bbaa9918208e7d1f4b5769f
Svetozar Stoev <zarkoto23@gmail.com>
2025-10-07 14:23:05 +0300

    add dynamic nav depend on logged in user

--- work-shop-routing-forms-game-play/client/src/components/header/Header.jsx
+++ work-shop-routing-forms-game-play/client/src/components/header/Header.jsx
@@ -1,6 +1,9 @@
+import { useContext } from "react";
 import { Link } from "react-router";
+import { UserContext } from "../../contexts/UserContext";
 
 export default function Header() {
+  const {email}=useContext(UserContext)
   return (
     <header>
       {/* <!-- Navigation --> */}
@@ -12,16 +15,21 @@ export default function Header() {
       <nav>
         <Link to="/games">All games</Link>
         {/* <!-- Logged-in users --> */}
-        <div id="user">
+        {
+        email?
+        (<div id="user">
           <Link to="/games/create">Create Game</Link>
           <Link to="/logout">Logout</Link>
-        </div>
-        {/* <!-- Guest users --> */}
+        </div>)
+        :
+        (
         <div id="guest">
           <Link to="/login">Login</Link>
           <Link to="/register">Register</Link>
-        </div>
+        </div>)
+}
       </nav>
     </header>
+        
   );
 }

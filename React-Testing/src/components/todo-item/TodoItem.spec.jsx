eccce771531f124775af758bd6bb72461be43c9d
Svetozar Stoev <zarkoto23@gmail.com>
2025-10-26 22:01:15 +0200

    add test

--- React-Testing/src/components/todo-item/TodoItem.spec.jsx
+++ React-Testing/src/components/todo-item/TodoItem.spec.jsx
@@ -1,10 +1,26 @@
-import { expect, it } from "vitest";
+import { beforeEach, expect, it } from "vitest";
 import TodoItem from "./TodoItem";
+import ReactDOM from "react-dom/client";
+import { act } from "react";
 
+beforeEach(() => {
+  document.body.innerHTML = "";
+});
 
+it("should display todo text", () => {
+  const container = document.createElement("div");
 
-it('should display todo text',()=>{
-    console.log(document);
-    expect(true).toBe(true)
-    
-})
+  document.body.appendChild(container);
+
+  const root = ReactDOM.createRoot(container);
+
+  act(() => {
+    root.render(<TodoItem text={"test1"} />);
+  });
+
+  //   console.log("eto go", container.innerHTML);
+
+  const textEl = document.querySelector("label span");
+
+  expect(textEl.textContent).toBe("test1");
+});

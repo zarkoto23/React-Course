9f3ec2455552e6e8374e35336d8bcec5c5da1133
Svetozar Stoev <zarkoto23@gmail.com>
2025-10-28 13:03:23 +0200

    add snapshot with real mocking data

--- React-Testing/src/components/todo-list/TodoList.spec.jsx
+++ React-Testing/src/components/todo-list/TodoList.spec.jsx
@@ -2,34 +2,27 @@ import { render, screen } from "@testing-library/react";
 import { expect, it, vi } from "vitest";
 import TodoList from "./TodoList";
 
-vi.mock('../../utils/request.js', () => {
-    return {
-        default: {
-            get: vi.fn(() => Promise.resolve({
-                'todo_2': {
-                    '_id': 'todo_2',
-                    'text': 'Do laundry',
-                    'isComplete': false
-                }
-            })),
-            post: vi.fn(),
-            put: vi.fn(),
-            delete: vi.fn(),
-            baseRequest: vi.fn()
-        }
-    };
+vi.mock("../../utils/request.js", () => {
+  return {
+    default: {
+      get: vi.fn(async () => {
+        return {
+          todo_2: {
+            _id: "todo_2",
+            text: "Do laundry",
+            isComplete: false,
+          },
+        };
+      }),
+    },
+  };
 });
+it("should check todo when click | Snapshot", async () => {
+  const { container } = render(<TodoList />);
 
-it('should check todo when click | Snapshot', async () => {
-    const { container } = render(<TodoList/>);
+  await vi.waitFor(() => {
+    expect(document.querySelector(".todo-item")).not.toBeNull();
+  });
 
-    await vi.waitFor(() => {
-        expect(document.querySelector('.todo-item')).not.toBeNull();
-    });
-
-    // Създаване на snapshot
-    expect(container.firstChild).toMatchSnapshot();
-    
-    // Или за целия container:
-    // expect(container).toMatchSnapshot();
+  expect(container.firstChild).toMatchSnapshot();
 });
